4405147436963e0d1264395a0d65724e91361753
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2020-03-02 21:56:57 -0300

    Criado variáveis para nomes das networks

--- src/config.py
+++ src/config.py
@@ -5,5 +5,10 @@ h1vm4 = "172.16.0.150"
 h2vm1 = "172.16.0.150"
 h2vm2 = "172.16.0.150"
 
+nwName_bridge = 'tcc-bridge'
+nwName_macvlan = 'tcc-macvlan'
+nwName_overlay = 'tcc-overlay'
+
+iteracoes = 2
 iperfTestDuration = 10
 sockperfTestDuration = 10
