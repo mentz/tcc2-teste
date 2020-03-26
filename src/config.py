ad695919cd6c0a2fd9a565a2aa7e02042a8f6977
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2020-03-26 01:30:43 +0000

    Alterar num. iteracoes e add. config p/ macvlan

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import docker
+
 h1vm1 = "172.16.25.15"
 h1vm2 = "172.16.25.12"
 h1vm3 = "172.16.25.16"
@@ -5,10 +7,74 @@ h1vm4 = "172.16.25.13"
 h2vm1 = "172.16.25.14"
 h2vm2 = "172.16.25.17"
 
+iteracoes = 1
+iperfTestDuration = 10
+sockperfTestDuration = 10
+
+###
+# Evite fazer modificações além daqui
+###
+
 nwName_bridge = 'tcc-bridge'
 nwName_macvlan = 'tcc-macvlan'
 nwName_overlay = 'tcc-overlay'
 
-iteracoes = 2
-iperfTestDuration = 10
-sockperfTestDuration = 10
+# Configuração para as redes macvlan - uma para cada host
+ipam_pool1 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.0/28',
+    gateway='124.42.0.254'
+  )
+ipam_pool2 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.16/28',
+    gateway='124.42.0.254'
+  )
+ipam_pool3 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.32/28',
+    gateway='124.42.0.254'
+  )
+ipam_pool4 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.48/28',
+    gateway='124.42.0.254'
+  )
+ipam_pool5 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.64/28',
+    gateway='124.42.0.254'
+  )
+ipam_pool6 = docker.types.IPAMPool(
+    subnet='124.42.0.0/24',
+    iprange='124.42.0.80/28',
+    gateway='124.42.0.254'
+  )
+
+ipam_config1 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool1]
+  )
+ipam_config2 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool2]
+  )
+ipam_config3 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool3]
+  )
+ipam_config4 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool4]
+  )
+ipam_config5 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool5]
+  )
+ipam_config6 = docker.types.IPAMConfig(
+    pool_configs=[ipam_pool6]
+  )
+
+ipam_config = [
+  ipam_config1,
+  ipam_config2,
+  ipam_config3,
+  ipam_config4,
+  ipam_config5,
+  ipam_config6,
+]
