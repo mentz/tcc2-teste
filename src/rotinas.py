44c3952118a3e84e857b8bcf04a16bb939fa0436
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2020-03-26 01:31:47 +0000

    Add macvlan e overlay

--- src/rotinas.py
+++ src/rotinas.py
@@ -7,12 +7,13 @@ import referencia as Referencia
 # Teste Referência
 def referencia(driver, cfgIndex, cfg, logDir):  
   if driver == 'host':
-    # Referencia.rodar_host(cfg[0], cfg[1], logMount)
     Referencia.rodar_host(cfg[0], cfg[1], logDir)
   elif driver == 'bridge':
     if (cfgIndex == 1):
-      # Referencia.rodar_bridge_cfg1(cfg[0], cfg[1], logMount)
       Referencia.rodar_bridge_cfg1(cfg[0], cfg[1], logDir)
     else:
-      # Referencia.rodar_bridge_cfg23(cfg[0], cfg[1], logMount)
       Referencia.rodar_bridge_cfg23(cfg[0], cfg[1], logDir)
+  elif driver == 'macvlan':
+    Referencia.rodar_macvlan(cfg[0], cfg[1], logDir)
+  elif driver == 'overlay':
+    Referencia.rodar_overlay(cfg[0], cfg[1], logDir)
