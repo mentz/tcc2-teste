0ede3be88830e3c9b2514dde6fca3ba1bcc1bec5
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2020-04-22 21:10:58 -0300

    Coleta de desempenho (mpstat)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TCC2 - Testes
 Este repositório contém os scripts usados nos testes desenvolvidos no meu TCC2.
+
+São usados até seis hospedeiros para realização dos testes.
+Nada impede o uso de um número menor mas os testes não serão comparáveis.
+
+Os scripts são feitos pensando que os hospedeiros terão nomes `dh-1` a `dh-6`.
+Caso necessário adapte isso no `run_benchmark.sh` e em `src/config.py`.
+
+Ferramentas necessárias nos hosts:
+- `Docker` configurado em modo Swarm
+- `sysstat` (usamos `mpstat`)
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -7,7 +7,25 @@ then
   echo 'Iniciando script...'
   rm -rf 'results'
   mkdir 'results'
+
+  # Garantir que não há mpstat de outra execução
+  for i in $(seq 1 6); do
+    ssh dh-$i "killall mpstat"
+  done
+
+  # Iniciar coleta de dados dos hospedeiros
+  for i in $(seq 1 6); do
+    ssh dh-$i "mpstat -o JSON 1 > ~/mpstat.json &"
+  done
+  
+  # Rodar bateria de testes
   python3 src/runTests.py
+
+  # Encerrar coleta de dados e agregar nessa pasta
+  for i in $(seq 1 6); do
+    ssh dh-$i "killall mpstat"
+    scp dh-$i:~/mpstat.json results/mpstat_dh-$i.json
+  done
 else
   echo 'Já existem resultados de outra execução.'
   echo 'Renomeie a pasta "results" para outro nome e rode novamente, ou use o'
